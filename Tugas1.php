28ffaf4c939f70c2a6837dea5c1c1773b0baff69
AAProject <87751322+asmajati15@users.noreply.github.com>
2021-09-02 15:39:54 +0700

    Add files via upload

---
+++ Tugas1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tugas 1 Asmajati Ananto</title>
+        <style>
+            .a1 {
+                width: 50%;
+                height: 100px;
+                background-color: red;
+                margin: auto;
+            }
+
+            .b1 {
+                width: 50%;
+                height: 500px;
+                background-color: black;
+                margin: auto;
+                text-align: center;
+                line-height: 500px;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+
+            b {
+                color: white;
+            }
+
+            .c1 {
+                width: 50%;
+                height: 100px;
+                background-color: red;
+                margin: auto;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="a1"></div>
+        <div class="b1">
+            <b>JIWAKU MEMBANTENG</b>
+        </div>
+        <div class="c1"></div>
+    </body>
+</html>
---
+++ Tugas2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="Tugas2.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>E-Ticketing Royal Marin</h1>
+            <form action="">
+                <label for=""><b>Username</b></label>
+                <input type="text" name="Masukkan Username" placeholder="Masukkan Username">
+                <label for=""><b>Password</b></label>
+                <input type="password" name="Masukkan Password" placeholder="Masukkan Password">
+                <button type="submit"><b>Sign in</b></button>
+                <button type="reset"><b>Reset</b></button>
+            </form>
+        </div>
+    </body>
+</html>
